d1656ec1be71030025f7deb3659017b9027298d8
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-04-18 01:58:52 +0530

    header added for dashboard

---
+++ src/pages/dashboard/society-master.tsx
@@ -0,0 +1,12 @@
+import Header from '@/commonComponents/header'
+import React from 'react'
+
+const SocietyMaster = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default SocietyMaster
